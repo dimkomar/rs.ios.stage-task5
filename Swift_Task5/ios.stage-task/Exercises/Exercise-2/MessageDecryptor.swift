a27dc66652e34ecd8947e1cc96148ed44c1da0c1
Dmitriy Komar <dmitriyko@playtika.com>
2021-06-14 22:37:03 +0200

    feat: implement logic of task_5_p1_2

--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -4,13 +4,20 @@ class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
         
-        var inputMessage = message
-        let pattern = "(\\d+\\[[a-z]+\\])"
+        let inputMessage = message
+        //let pattern = "(\\d+\\[[a-z]+\\])"
         
 
-        let regex = try! NSRegularExpression(pattern: "[a-z]", options: [])
-        if (regex.firstMatch(in: inputMessage, options: [], range: NSMakeRange(0, inputMessage.utf16.count)) != nil){
+        let regexOne = try! NSRegularExpression(pattern: "[a-z]", options: [])
+        let regexTwo = try! NSRegularExpression(pattern: ".*[^A-Za-z0-9].*", options: [])
+        
+        if (message.first == Optional("0")) { return ""}
+        
+        if (regexOne.firstMatch(in: inputMessage, options: [], range: NSMakeRange(0, inputMessage.utf16.count)) != nil){
             return inputMessage
+            
+        } else if (regexTwo.firstMatch(in: inputMessage, options: [], range: NSMakeRange(0, inputMessage.utf16.count)) != nil){
+            
         }
         
 
