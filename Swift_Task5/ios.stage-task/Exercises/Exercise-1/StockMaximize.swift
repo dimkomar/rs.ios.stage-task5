adbbb5b588780dc4e2d42df4c659aabdda564595
Dmitriy Komar <dmitriyko@playtika.com>
2021-06-14 21:58:09 +0200

    feat: implement logic of task_5_p1_2

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,29 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var valueStocks : Int = 0
+        
+        if prices.count == 0 || prices.count == 1 || prices.dropLast().allSatisfy({ $0 == prices.last }) {
+            return 0
+        } else {
+            var mutablePrices = prices
+            var max = mutablePrices.max() ?? 0
+            for value in prices {
+             //   print("Значение акции сегодня: \(value)")
+             //   print("Значение ближайшего максимума: \(max)")
+                if (value < max && mutablePrices.firstIndex(of: value)! < mutablePrices.firstIndex(of: max)!){
+                //    print("Добавлено: \(max - value)")
+                    valueStocks += max - value
+                } else if (value == max) {
+                    let indexValueForRemove = mutablePrices.firstIndex(of: max)
+                    mutablePrices.remove(at:indexValueForRemove!)
+                    max = mutablePrices.max() ?? 0
+                }
+              //  print("Значение в стоке : \(valueStocks)")
+            }
+            //print("Финальное значение в стоке : \(valueStocks)")
+            return valueStocks
+        }
+        
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,20 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
+        
+        var inputMessage = message
+        let pattern = "(\\d+\\[[a-z]+\\])"
+        
+
+        let regex = try! NSRegularExpression(pattern: "[a-z]", options: [])
+        if (regex.firstMatch(in: inputMessage, options: [], range: NSMakeRange(0, inputMessage.utf16.count)) != nil){
+            return inputMessage
+        }
+        
+
+        
+        
+        
         return ""
     }
 }
